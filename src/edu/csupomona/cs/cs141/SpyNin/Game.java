839f66c829f2e7407e65df7da22d0eafb4d26ab9
David Khacherian <dmkhacherian@csupomona.edu>
2012-02-15 13:31:31 -0800

    Changed Game

--- src/edu/csupomona/cs/cs141/SpyNin/Game.java
+++ src/edu/csupomona/cs/cs141/SpyNin/Game.java
@@ -1,14 +1,6 @@
 /**
- * 
- */
-package edu.csupomona.cs.cs141.SpyNin;
-
-/**
- * *
  * CS 141: Introduction to Programming and Problem Solving
  * Professor: Edwin Rodr&iacute;guez
- *
- * Programming Assignment #N
  *
  * Spies vs. Ninjas 
  *
@@ -18,19 +10,78 @@ package edu.csupomona.cs.cs141.SpyNin;
  *   
  * This is the main class in which the game runs.
  */
+
+package edu.csupomona.cs.cs141.SpyNin;
+import java.util.Scanner;
+
 public class Game {
 	
 	/**
-	 * This boolean is initialized to determine whether a game is won
-	 * or lost, will determine certain actions only in this class.
+	 * Whether or not the game has been won.
 	 */
 	public boolean winGame;
+    private static Scanner keyboard = new Scanner(System.in);
 
 	/**
 	 * The main game loop. <Further explanation of implementation>
 	 * @param args
 	 */
-	public void Run(){
-	
-	}
-}
+	public static void main(String[] args){
+        boolean quit=false;
+        int mainMenu=startMenu();
+        while (quit == false){
+        switch (mainMenu){
+        case 1:{
+                while(true){
+                        boolean play = startGame();
+                }
+                    
+                
+        }
+        case 2:{
+                
+        }
+            
+        case 3:{
+                
+        }
+            
+        default:{
+                
+        }
+        }
+        }
+    }
+        
+    public static int startMenu(){
+        System.out.println("1. New Game");
+        System.out.println("1. Load Game");
+        System.out.println("1. Quit");
+        System.out.print("Choice: ");
+        int choice = keyboard.nextInt();
+        return choice;
+    }
+        
+    playGame(){
+            
+    }
+    public boolean startGame(){
+        return play;
+    }
+        
+    public int playerMenu(){
+        return choice;
+    }
+        
+    public void Win(){
+                
+    }
+        
+    public void Lose(){
+                
+    }
+        
+    public Board ninjaTurn(Board board){
+        return board;
+    }
+}}
